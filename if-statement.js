1078772207ed5ff483de4f734ba70ed2860afefb
Juancortes99 <sajuma22@gmail.com>
2023-08-15 23:21:15 -0500

    bloque condicional terminado

---
+++ if-statement.js
@@ -0,0 +1,6 @@
+let fruit = "orange";
+if (fruit.length > 5) {
+    console.log("the fruit name has more than 5 characters");
+} else {
+    console.log("the fruit name has five characters or less");
+}
